b7026d01e72eb972463ca2fdaee1a25c2b4ccf7a
Jeroen Janssen <jeroen.janssen@gmail.com>
2024-12-26 14:09:01 +0100

    navbar blog first

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,17 +75,18 @@ const config: Config = {
         src: 'img/logo.svg',
       },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
+
         {to: '/', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/japj',
           label: 'GitHub',
           position: 'right',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
         },
       ],
     },
