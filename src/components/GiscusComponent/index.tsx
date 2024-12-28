5e55633d7bcca81dd9fc239c25dfb3a75beca9f9
Jeroen Janssen <jeroen.janssen@gmail.com>
2024-12-28 14:07:40 +0100

    add GiscusComponent

---
+++ src/components/GiscusComponent/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Giscus from "@giscus/react";
+import { useColorMode } from '@docusaurus/theme-common';
+
+export default function GiscusComponent() {
+    const { colorMode } = useColorMode();
+
+    return (
+        <Giscus
+            repo="japj/japj.github.io"
+            repoId="R_kgDONigtoQ"
+            category="Giscus Comments"
+            categoryId="DIC_kwDONigtoc4ClkfH"
+            mapping="pathname"
+            strict="0"
+            reactionsEnabled="1"
+            emitMetadata="1"
+            inputPosition="bottom"
+            theme={colorMode}
+            lang="en"
+            loading="lazy"
+            crossorigin="anonymous"
+            async
+            />
+        );
+}
