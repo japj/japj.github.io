d8d410da72f6076b8e7dac166609ce9d593d1964
Jeroen Janssen <jeroen.janssen@gmail.com>
2025-03-03 18:55:01 +0000

    Revert "main title is Blog"
    This reverts commit c880b1c6a0941f9a466a832da4670a6166a5fcb4.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Blog',
+  title: 'japj.net',
   tagline: 'Personal blog & docs',
   favicon: 'favicon.ico',
 
