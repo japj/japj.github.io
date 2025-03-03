c880b1c6a0941f9a466a832da4670a6166a5fcb4
Jeroen Janssen <jeroen.janssen@gmail.com>
2025-03-03 18:33:45 +0000

    main title is Blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'japj.net',
+  title: 'Blog',
   tagline: 'Personal blog & docs',
   favicon: 'favicon.ico',
 
