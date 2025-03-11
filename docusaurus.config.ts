a0670e53c0239e7acd201c91b26a03d65cc04d27
Jeroen Janssen <jeroen.janssen@gmail.com>
2025-03-11 15:43:59 +0000

    idebarCollapsed: false

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,6 +41,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/japj/japj.github.io/tree/main/',
           showLastUpdateTime: true,
+          sidebarCollapsed: false,
         },
         blog: {
           routeBasePath: "/",
